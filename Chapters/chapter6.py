72e087c0bb6c2ae3e9b5e05b0b57849e5be4b7f8
Halfgods <justinroy9702@gmail.com>
2025-12-01 22:04:07 +0530

    Shape Detection

--- Chapters/chapter6.py
+++ Chapters/chapter6.py
@@ -14,10 +14,29 @@ def getContours(img):
     for cnt in contours:
         area = cv.contourArea(cnt)
         print(area)
-        cv.drawContours(contourimg , cnt , -1 , (255,0,0), 3)
-        
+        if area>500:
+            cv.drawContours(contourimg , cnt , -1 , (255,0,0), 3)
+            perimeter = cv.arcLength(cnt , True)
+            print(perimeter , "perimeter")
+            approx = cv.approxPolyDP(cnt , 0.02*perimeter , True)
+            print(len(approx) , "approx len")
+            objCor = len(approx)
+            x , y , w , h = cv.boundingRect(approx)
+            centerX = x + (w // 2)
+            centerY = y + (h // 2)
+            if objCor == 3:
+                objectType = "Triangle"
+                cv.putText(contourimg , objectType , (centerX , centerY) , cv.FONT_HERSHEY_COMPLEX , 0.5 , (0,0,0) ,2)
+            elif objCor == 4:
+                cv.putText(contourimg , "Square" , (centerX , centerY) , cv.FONT_HERSHEY_COMPLEX , 0.5 , (0,0,0) ,2)
+            else:
+                cv.putText(contourimg , "Polygon" , (centerX , centerY) , cv.FONT_HERSHEY_COMPLEX , 0.5 , (0,0,0) ,2)
+            cv.rectangle(contourimg,(x,y),(x+w , y+h) , (0,255,0) ,2)
+            
+        else:
+            continue
 getContours(canny) # Just run it. Don't assign it.
-stack = stackImages(0.5, ([img, gray,thresh],[blur, contourimg,canny]))
+stack = stackImages(0.6, ([img,contourimg]))
 cv.imshow("Stack", stack)
 cv.waitKey(0)
 cv.destroyAllWindows()

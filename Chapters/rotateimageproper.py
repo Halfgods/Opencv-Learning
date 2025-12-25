da2c0bf6e5a21049bd26c9e8cf677f4d5ff46d1b
Halfgods <justinroy9702@gmail.com>
2025-12-25 21:11:00 +0530

    Rectified my errors

--- Chapters/rotateimageproper.py
+++ Chapters/rotateimageproper.py
@@ -3,17 +3,23 @@ import numpy as np
 import argparse
 import imutils
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="Path to the input image")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required=True,
+#                 help="Path to the input image")
+# args = vars(ap.parse_args())
 
 
-img = cv.imread(args["image"])
-for angle in np.arange(0,360 , 15):
-    image = cv.getRotationMatrix2D((img.shape[1]//2 , img.shape[0]//2) , angle , 1.0)
-    cv.imshow("Rotate1", image)
-    cv.waitKey(200)
+img = cv.imread("./Data/tablets.png")
+try:
+    for angle in np.arange(0,360 , 15):
+# Calculate the Matrix
+        M = cv.getRotationMatrix2D((img.shape[1]//2, img.shape[0]//2), angle, 1.0)
+        # Apply the Matrix to the image
+        rotated_img = cv.warpAffine(img, M, (img.shape[1], img.shape[0]))
+        cv.imshow("Rotate1", rotated_img)
+        cv.waitKey(200)
+except:
+    print("Error in rotation using cv.getRotationMatrix2D")
     
 for angle in np.arange(0,360,15):
     image = imutils.rotate_bound(img , angle)
